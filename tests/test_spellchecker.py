6e9575b198d1e6dc1f16c3b3596acf020732bbea
Amin Etesamian <aminetesamian1371@gmail.com>
2017-08-29 18:08:47 -0700

    fixes issue in db_path

--- tests/test_spellchecker.py
+++ tests/test_spellchecker.py
@@ -1,11 +1,15 @@
 import asyncio
+from os import path
+
 import aiofiles
 
 from async_faspell.main import SpellChecker
+import tests
 
+PATH_TO_DB = path.abspath(path.join(path.dirname(tests.__file__), 'db'))
 
 async def test_spell_checker():
-    async with aiofiles.open('db', 'r') as db:
+    async with aiofiles.open(PATH_TO_DB, 'r') as db:
         sp = SpellChecker(await db.read())
         assert await sp.correct('متغاضی') == ['متقاضی']
 
